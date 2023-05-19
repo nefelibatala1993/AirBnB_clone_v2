6ee46438fef0754a4ca615a89203e4428f90532a
nefelibatala1993 <leuela1993@gmail.com>
2023-05-19 21:52:24 +0300

    add task #6

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Start a flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text: str):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python")
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n: int):
+    return str(n) + " is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template_route(n: int):
+    return render_template("5-number.html", number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
