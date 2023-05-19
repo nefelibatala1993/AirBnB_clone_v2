9fe5e68545049efb1664961589476c722dc2714f
nefelibatala1993 <leuela1993@gmail.com>
2023-05-19 11:57:45 +0300

    Added function documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """Returns the home page"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Returns the hbnb page"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
+    """Returns a custom page"""
     return f"C {text.replace('_', ' ')}"
 
 
