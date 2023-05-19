d8e56f18ecba53f37578fe2498edca77d490b19b
nefelibatala1993 <leuela1993@gmail.com>
2023-05-19 21:58:12 +0300

    update task#6

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def number_route(n: int):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template_route(n: int):
-    return render_template("5-number.html", n=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == '__main__':
